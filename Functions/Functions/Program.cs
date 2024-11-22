78f6d83d28d8091067f294d11c74e04099f61344
Adem-ma <37601@ma-web.nl>
2024-11-22 12:22:24 +0100

    functionMapAdded

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Program program = new Program();
+            //program variable ig bro idk
+           program.Run();
+            //we gebruiken hier de function Run, dit laat het RUNNENNNN
+        }
+        internal void Run()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma");
+        }
+
+    }
+}
