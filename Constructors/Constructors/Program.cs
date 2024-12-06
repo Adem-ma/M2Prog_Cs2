b60ece15b5b5c8e7bfcc17a899b3b31208797ad2
Adem-ma <37601@ma-web.nl>
2024-12-06 15:28:11 +0100

    constructorsCommit1

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        private void Run()
+        {
+            QuizVraag quizVraag = new QuizVraag("wa 9 plu 10", "21?");
+            Quiz quiz = new Quiz(10);
+        }
+    }
+}
---
+++ Constructors/Constructors/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+     class Quiz
+    {
+        internal QuizVraag[] vragen;
+        internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+        internal int aantalVragen;
+
+        internal Quiz()
+        {
+            vragen = new QuizVraag[aantalVragen];
+            ingevuldeAntwoorden = new QuizVraagAntwoord[aantalVragen];
+        }
+    }
+}
